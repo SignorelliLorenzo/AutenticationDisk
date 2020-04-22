b9aadbf23e43363a7620582bcef70efd7930e102
GhilardiAndrea <62284496+GhilardiAndrea@users.noreply.github.com>
2020-04-22 21:55:46 +0200

    Funzione scraping data iniziale

--- AutenticationDisk/Web Scrapping.cs
+++ AutenticationDisk/Web Scrapping.cs
@@ -35,5 +35,42 @@ namespace Web_Scrapping
 
             return immagine;
         }
+
+        public static int Data(string titolo)
+        {
+            int data = default;
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc;
+            doc = web.Load("https://it.wikipedia.org/wiki/Breaking_Bad#Episodi");
+
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@title]"))
+            {
+                HtmlAttribute att = link.Attributes["title"];
+                if (att.Value.Contains("#"))
+                {
+                    string[] substring = att.Value.Split("#");
+
+
+
+                }
+                else
+                {
+
+                    try
+                    {
+                        data = int.Parse(att.Value);
+                        break;
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+            }
+
+            return data;
+        }
+    
     }
 }
