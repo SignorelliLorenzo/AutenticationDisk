6c5a3b81646e1d2afcc955ea3f829c3d77b3ec13
GhilardiAndrea <62284496+GhilardiAndrea@users.noreply.github.com>
2020-04-22 12:45:21 +0200

    Funzione immagine iniziale

--- AutenticationDisk/Web Scrapping.cs
+++ AutenticationDisk/Web Scrapping.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace Web_Scrapping
 {
     class Reserch
     {
+        public static string Immagine(string titolo)
+        {
+            string immagine = default;
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc;
+            doc = web.Load("https://it.wikipedia.org/wiki/Breaking_Bad#Episodi");
 
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//img[@src]"))
+            {
+                HtmlAttribute att = link.Attributes["src"];
+                if (att.Value.Contains("#"))
+                {
+                    string[] substring = att.Value.Split('#');
+
+                    immagine = substring[0];
+
+                }
+                else
+                {
+
+                }
+                break;
+            }
+
+            return immagine;
+        }
     }
 }
